17bd519a4f13fe837fee4425420b16d20e3da5bf
Omid Aladini <omidaladini@gmail.com>
2014-11-29 15:23:17 +0100

    Add list of programms as hardcoded list

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,15 @@
 package com.omidaladini.bbcprogrammedispatch
 
 object BBCProgrammeDispatch {
+
+  val programFeeds = List(
+    "http://www.bbc.co.uk/radio2/programmes/schedules.json",
+    "http://www.bbc.co.uk/5livesportsextra/programmes/schedules.json",
+    "http://www.bbc.co.uk/worldserviceradio/programmes/schedules.json")
+
   def main(args: Array[String]) {
-    println("Hello World!!!")
+
+
   }
 }
 
