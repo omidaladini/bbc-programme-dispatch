71b18d83a6715381d80e00fded5b2bb8460c0f02
Omid Aladini <omidaladini@gmail.com>
2014-11-29 18:06:31 +0100

    Move scheduler to its own home

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,61 +1,8 @@
 package com.omidaladini.bbcprogrammedispatch
 
-import org.joda.time._
-
 object BBCProgrammeDispatch {
 
-  import Programmes._
-  import com.twitter.util._
-  import com.twitter.util.Time._
-  import com.twitter.conversions.time._
-
-  val timer = new JavaTimer
-  val onAirNextIntervals = List(10, 5, 3, 2, 1)
-
-  def scheduleBroadcast(list: List[Programme]) {
-
-    list.foreach( prog => {
-      val format = new com.twitter.util.TimeFormat("yyyy-MM-dd'T'HH:mm:ss")
-      val time = format.parse(prog.start)
-      val sinceNow = time.sinceNow.inNanoseconds / 1000000
-
-      if(sinceNow > 0) {
-        timer.doAt(Time.now + sinceNow.millis) {
-          DAB.send_onairnow(prog.pid, "On Air Now")
-        }
-      }
-
-      onAirNextIntervals.foreach(interval => {
-        val sinceNowNext = sinceNow - interval * 60 * 1000
-        if(sinceNowNext > 0) {
-          timer.doAt(Time.now + sinceNowNext.millis) {
-            DAB.send_onairnext(prog.pid, "On Air Next")
-          }
-        }
-      })
-
-    })
-  }
-
-  def scheduleToday = {
-    programFeeds.foreach( p => {
-      scheduleBroadcast(loadTodaysProgramme(p))
-    })
-  }
-
-  val dayMillis = 86400 * 1000
-
-  def scheduleTomorrow : Unit = {
-    programFeeds.foreach( p => {
-      scheduleBroadcast(loadTodaysProgramme(p))
-    })
-
-    //TODO: day-boundary race conditions ?
-    timer.doAt(Time.now + dayMillis.millis) {
-      scheduleTomorrow
-    }
-
-  }
+  import Scheduler._
 
   def main(args: Array[String]) {
 
---
+++ src/main/scala/Scheduler.scala
@@ -0,0 +1,61 @@
+package com.omidaladini.bbcprogrammedispatch
+
+object Scheduler {
+
+  import Programmes._
+  import com.twitter.util._
+  import com.twitter.util.Time._
+  import com.twitter.conversions.time._
+  import org.joda.time._
+
+  val timer = new JavaTimer
+  val onAirNextIntervals = List(10, 5, 3, 2, 1)
+
+  def scheduleBroadcast(list: List[Programme]) {
+
+    list.foreach( prog => {
+      val format = new com.twitter.util.TimeFormat("yyyy-MM-dd'T'HH:mm:ss")
+      val time = format.parse(prog.start)
+      val sinceNow = time.sinceNow.inNanoseconds / 1000000
+
+      if(sinceNow > 0) {
+        timer.doAt(Time.now + sinceNow.millis) {
+          DAB.send_onairnow(prog.pid, "On Air Now")
+        }
+      }
+
+      onAirNextIntervals.foreach(interval => {
+        val sinceNowNext = sinceNow - interval * 60 * 1000
+        if(sinceNowNext > 0) {
+          timer.doAt(Time.now + sinceNowNext.millis) {
+            DAB.send_onairnext(prog.pid, "On Air Next")
+          }
+        }
+      })
+
+    })
+  }
+
+  def scheduleToday = {
+    programFeeds.foreach( p => {
+      scheduleBroadcast(loadTodaysProgramme(p))
+    })
+  }
+
+  val dayMillis = 86400 * 1000
+
+  def scheduleTomorrow : Unit = {
+    programFeeds.foreach( p => {
+      scheduleBroadcast(loadTodaysProgramme(p))
+    })
+
+    //TODO: day-boundary race conditions ?
+    timer.doAt(Time.now + dayMillis.millis) {
+      scheduleTomorrow
+    }
+
+  }
+
+
+
+}
